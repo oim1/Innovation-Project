75ef8f4feb7acf104e9049b4de474f648f5edcf6
minhphan6624 <quangminh6624@gmail.com>
2023-08-29 15:59:43 +1000

    Added social icons and css for footer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import "../styles/Home.css"
+import Footer from "../components/Footer";
+import "../styles/Home.css";
 
 const Home = () => {
-    return (
-        <div className="HomePage">
-            <body>
-                <Navbar />
-                <h1 id="MainText">Buy and Exchange<br/>Crypto Tokens Instantly</h1>
-            </body>
-        </div>
-    )
-}
+  return (
+    <div className="HomePage">
+      <body>
+        <Navbar />
+        <h1 id="MainText">
+          Buy and Exchange
+          <br />
+          Crypto Tokens Instantly
+        </h1>
+      </body>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
