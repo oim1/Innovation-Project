317001429d4fcaa74fd6f1a3ecb937f53886c378
minhphan6624 <quangminh6624@gmail.com>
2023-08-31 13:09:25 +1000

    Adding the login page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
-import logo from "../../public/assets/logos/coinwave.png"
+import logo from "../../public/assets/logos/coinwave.png";
 
 const Navbar = () => {
-    return (
-        <div className='navbar'>
-            <div className='leftSide'>
-                <Link to={"/"}>
-                    <img src={logo} alt="coinwave logo"/>
-                </Link>
-            </div>
-            <div className="rightSide">
-                <Link to={"/"}>Home</Link>
-                <Link to={"/assetspage"}>Assets</Link>
-                <Link to={"/about"}>About Us</Link>
-                <Link to={"/transactions"}>Transactions</Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <div className="leftSide">
+        <Link to={"/"}>
+          <img src={logo} alt="coinwave logo" />
+        </Link>
+      </div>
+      <div className="rightSide">
+        <Link to={"/"}>Home</Link>
+        <Link to={"/assetspage"}>Assets</Link>
+        <Link to={"/about"}>About Us</Link>
+        <Link to={"/transactions"}>Transactions</Link>
+        <Link to={"/login"}>
+          {" "}
+          <button> Log in </button>{" "}
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="Search">
+      <div className="SearchBar">
+        <input
+          id="searchQueryInput"
+          type="text"
+          name="searchQueryInput"
+          placeholder="Search"
+        />
+        <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit">
+          <img
+            src={searchIcon}
+            style={{ width: 24, height: 24 }}
+            alt="searchIcon"
+          />
+        </button>
+      </div>
+      <hr id="line" />
+      <h1 id="filter">Filter</h1>
+      <input
+        id="locationQueryInput"
+        type="text"
+        name="searchQueryInput"
+        placeholder="Location"
+      />
+      <a id="googlemaps" href="https://www.google.com/maps/">
+        Google Maps
+      </a>
+      <h2>Price Range</h2>
+      <Slider
+        value={value}
+        onChange={rangeSelector}
+        valueLabelDisplay="auto"
+        max={10000}
+      />
+      {value[0]} - {value[1]}
+      <hr id="line" />
+      <h1 id="filter">Categories</h1>
+      <button className="categoryButton" type="button">
+        Property
+      </button>
+      <button className="categoryButton" type="button">
+        Health & Care
+      </button>
+      <button className="categoryButton" type="button">
+        Groceries
+      </button>
+      <button className="categoryButton" type="button">
+        Vehicles
+      </button>
+      <button className="categoryButton" type="button">
+        Electronics
+      </button>
+      <button className="categoryButton" type="button">
+        NFTs
+      </button>
+    </div>
+  );
+}
+
+export default Sidebar;
