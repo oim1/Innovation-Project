9c12077eb822d1b4906043329e1fe7338789d7be
Minh Phan <quangminh6624@gmail.com>
2023-10-14 23:14:13 +1100

    Responsive Sidebar

--- src/pages/AssetsPage.js
+++ src/pages/AssetsPage.js
@@ -1,8 +1,10 @@
 import React, {useContext, useEffect, useState} from "react";
-import "../styles/pages/AssetsPage.css";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { createClient } from "@supabase/supabase-js";
-import { Slider } from "@mui/material";
+
 import "../styles/components/Sidebar.css";
+import "../styles/pages/AssetsPage.css";
+
 import searchIcon from "../../public/assets/images/search-svgrepo-com.svg";
 
 const supabase = createClient("https://bjihaznrhkskpfiyimdr.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqaWhhem5yaGtza3BmaXlpbWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5MDE0ODQsImV4cCI6MjAxMjQ3NzQ4NH0.2gJLsyOSXl2uLEZ0vJO3xWr7Kod3ddtqfkmR8tiM4J8", { db: { schema: "Marketplace"}});
@@ -15,61 +17,56 @@ const Assets = () => {
   }
 
   const [products, setProducts] = useState([]);
-  const [value, setValue] = useState([0, 100]);
   const [category, setCategory] = useState("")
   const [isSelected, setSelected] = useState(false)
 
+  // // Add to cart
+  // const [cartItems, setCartItems] = useState([]);
+
+  // useEffect(() => {
+  //   const storedCartItems = JSON.parse(localStorage.getItem("cartItems"));
+  //   if (storedCartItems) {
+  //     setCartItems(storedCartItems);
+  //   }
+  // }, []);
+
+  // function addHandler(e, item) {
+  //   e.preventDefault();
+  //   const existingItem = cartItems.find((cartItem) => cartItem.name === item.name);
+
+  //   if (existingItem) {
+  //     const updatedCartItems = cartItems.map((cartItem) =>
+  //       cartItem.name === existingItem.name ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+  //     );
+  //     setCartItems(updatedCartItems);
+  //     localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
+  //   } 
+  //   else {
+  //     setCartItems([...cartItems, { ...item, quantity: 1 }]);
+  //   }
+
+  // }
+
+  // function updateQuantity(item, quantityChange) {
+  //   const updatedCartItems = cartItems.map((cartItem) => {
+  //     if (cartItem.name === item.name) {
+  //       const newQuantity = cartItem.quantity + quantityChange;
+  //       return newQuantity > 0 ? { ...cartItem, quantity: newQuantity } : cartItem;
+  //     }
+  //     return cartItem;
+  //   });
+
+  //   setCartItems(updatedCartItems);
+  //   localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
+  // }
+
+  // function removeFromCart(item) {
+  //   const updatedCartItems = cartItems.filter((cartItem) => cartItem.name !== item.name);
+  //   setCartItems(updatedCartItems);
+  //   localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
+  // }
+
   // Add to cart
-  const [cartItems, setCartItems] = useState([]);
-
-  useEffect(() => {
-    const storedCartItems = JSON.parse(localStorage.getItem("cartItems"));
-    if (storedCartItems) {
-      setCartItems(storedCartItems);
-    }
-  }, []);
-
-  function addHandler(e, item) {
-    e.preventDefault();
-    const existingItem = cartItems.find((cartItem) => cartItem.name === item.name);
-
-    if (existingItem) {
-      const updatedCartItems = cartItems.map((cartItem) =>
-        cartItem.name === existingItem.name ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
-      );
-      setCartItems(updatedCartItems);
-      localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
-    } 
-    else {
-      setCartItems([...cartItems, { ...item, quantity: 1 }]);
-    }
-
-  }
-
-  function updateQuantity(item, quantityChange) {
-    const updatedCartItems = cartItems.map((cartItem) => {
-      if (cartItem.name === item.name) {
-        const newQuantity = cartItem.quantity + quantityChange;
-        return newQuantity > 0 ? { ...cartItem, quantity: newQuantity } : cartItem;
-      }
-      return cartItem;
-    });
-
-    setCartItems(updatedCartItems);
-    localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
-  }
-
-  function removeFromCart(item) {
-    const updatedCartItems = cartItems.filter((cartItem) => cartItem.name !== item.name);
-    setCartItems(updatedCartItems);
-    localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
-  }
-
-  // Add to cart
-  
-  const rangeSelector = (event, newValue) => {
-    setValue(newValue);
-  };
 
   useEffect(() => {
     getProducts();
@@ -91,10 +88,17 @@ const Assets = () => {
     console.log(category)
   };
 
+  const [openSidebar, setOpenSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setOpenSidebar(!openSidebar);
+  };
+
   return (
     <>
       <div className="AssetsPage">
-        <div className="Sidebar">
+        <div className="Sidebar" id={openSidebar ? "open" : "close"}>
+          <ArrowForwardIosIcon className="arrowButton" onClick={toggleSidebar}/>
           <div className="SearchBar">
             <input
                 id="searchQueryInput"
@@ -104,7 +108,7 @@ const Assets = () => {
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
             />
-            <button
+            {/* <button
                 id="searchQuerySubmit"
                 type="submit"
                 name="searchQuerySubmit"
@@ -115,45 +119,39 @@ const Assets = () => {
                   style={{ width: 24, height: 24 }}
                   alt="searchIcon"
               />
-            </button>
+            </button> */}
           </div>
-          <hr id="line" />
-          {/* Price selection section */}
-          <h2>Price Range</h2>
-          <Slider
-              value={value}
-              onChange={rangeSelector}
-              valueLabelDisplay="auto"
-              max={10000}
-          />
-          {value[0]} - {value[1]}
-          {/* End of price section */}
-          <hr id="line" />
+         
           {/* Categories selection section */}
-          <h1 id="filter">Categories</h1>
-          <div>
+          <div className="Categories">
+            <h1>Categories</h1>
             <label className="sidebar-label-container">
-              <input type="radio" value="" name="test" onChange={e => (categoryHandler(e))}/>
+              <input type="radio" value="" name="categories" onChange={e => (categoryHandler(e))}/>
+              <span class="checkmark"></span>
               All
             </label>
 
             <label className="sidebar-label-container">
-              <input type="radio" value="1" name="test" onChange={e => (categoryHandler(e))} />
+              <input type="radio" value="1" name="categories" onChange={e => (categoryHandler(e))} />
+              <span class="checkmark"></span>
               Vehicles
             </label>
 
             <label className="sidebar-label-container">
-              <input type="radio" value="2" name="test" onChange={e => (setCategory(e.target.value))}/>
+              <input type="radio" value="2" name="categories" onChange={e => (setCategory(e.target.value))}/>
+              <span class="checkmark"></span>
               Electronics
             </label>
 
             <label className="sidebar-label-container">
-              <input type="radio" value="3" name="test" onChange={e => (setCategory(e.target.value))}/>
+              <input type="radio" value="3" name="categories" onChange={e => (setCategory(e.target.value))}/>
+              <span class="checkmark"></span>
               Animals
             </label>
 
             <label className="sidebar-label-container">
-              <input type="radio" value="4" name="test" onChange={e => (setCategory(e.target.value))}/>
+              <input type="radio" value="4" name="categories" onChange={e => (setCategory(e.target.value))}/>
+              <span class="checkmark"></span>
               Food & Drinks
             </label>
           </div>
